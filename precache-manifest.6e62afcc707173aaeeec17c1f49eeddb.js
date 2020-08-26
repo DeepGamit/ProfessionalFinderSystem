57063dfe03d66902647285136274e2f68b03acb2
Deep Gamit <gamit.d@husky.neu.edu>
2020-08-26 13:02:20 -0400

    Updates

---
+++ precache-manifest.6e62afcc707173aaeeec17c1f49eeddb.js
@@ -0,0 +1,342 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adf734d5d0c2ac5293b220d85d58c0f3",
+    "url": "/WebDesignProject/index.html"
+  },
+  {
+    "revision": "f9f9ec99ddecb61688e6",
+    "url": "/WebDesignProject/static/css/main.b9d5015a.chunk.css"
+  },
+  {
+    "revision": "d90dea5c38057b1e5a65",
+    "url": "/WebDesignProject/static/js/2.618b27f0.chunk.js"
+  },
+  {
+    "revision": "50da79815d3a6870141f540e60659a89",
+    "url": "/WebDesignProject/static/js/2.618b27f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9f9ec99ddecb61688e6",
+    "url": "/WebDesignProject/static/js/main.f0347f18.chunk.js"
+  },
+  {
+    "revision": "5dd2500004aae15cd0ae",
+    "url": "/WebDesignProject/static/js/runtime-main.ecca9882.js"
+  },
+  {
+    "revision": "7b81a1f94c3132f53e012ea9cdd73907",
+    "url": "/WebDesignProject/static/media/ac.7b81a1f9.jpg"
+  },
+  {
+    "revision": "9a610f4332650ad10341c9c07a032b5a",
+    "url": "/WebDesignProject/static/media/air-conditioner.9a610f43.png"
+  },
+  {
+    "revision": "8ccd3414dfbf0bce8e84d196251b59f5",
+    "url": "/WebDesignProject/static/media/appliance1.8ccd3414.jpg"
+  },
+  {
+    "revision": "e1cd63595bbe21580ec8868a7397228b",
+    "url": "/WebDesignProject/static/media/appliance2.e1cd6359.png"
+  },
+  {
+    "revision": "5dd80d73341f886bf138f5e789ace2d8",
+    "url": "/WebDesignProject/static/media/appliance3.5dd80d73.png"
+  },
+  {
+    "revision": "f3b13c0e7a3d5ca3d5eea6859dd88242",
+    "url": "/WebDesignProject/static/media/banner.f3b13c0e.jpg"
+  },
+  {
+    "revision": "bfce3e805b51017c5f3c68af274f4bfc",
+    "url": "/WebDesignProject/static/media/clean.bfce3e80.png"
+  },
+  {
+    "revision": "b12d80015d28ee7044319726d72f6ca4",
+    "url": "/WebDesignProject/static/media/cleaning.b12d8001.jpg"
+  },
+  {
+    "revision": "8e557bf405f7fb2f205a63df18fe46d0",
+    "url": "/WebDesignProject/static/media/cleaning1.8e557bf4.jpeg"
+  },
+  {
+    "revision": "2992b65019e7ec7e0acb47d3440e1790",
+    "url": "/WebDesignProject/static/media/cleaning2.2992b650.jpg"
+  },
+  {
+    "revision": "497861e381f7ba182aca02bf5d8c6bc6",
+    "url": "/WebDesignProject/static/media/cleaning3.497861e3.jpeg"
+  },
+  {
+    "revision": "f3b8c771c5bba6ff063bc4516ea0c099",
+    "url": "/WebDesignProject/static/media/cleaning4.f3b8c771.jpeg"
+  },
+  {
+    "revision": "fc721a4c2ea03548bd7f40a6eec0dc4b",
+    "url": "/WebDesignProject/static/media/cleaning5.fc721a4c.jpg"
+  },
+  {
+    "revision": "f89b3f9f0985e96d53214416695914cc",
+    "url": "/WebDesignProject/static/media/cleaning6.f89b3f9f.jpeg"
+  },
+  {
+    "revision": "87d0304b8b554772c4699324d988accf",
+    "url": "/WebDesignProject/static/media/cp.87d0304b.jpg"
+  },
+  {
+    "revision": "6f633ae55c84dea3d1a62bdd1575e2a1",
+    "url": "/WebDesignProject/static/media/el.6f633ae5.jpg"
+  },
+  {
+    "revision": "5d790290e767a5c216512917b756745f",
+    "url": "/WebDesignProject/static/media/electrician1.5d790290.jpg"
+  },
+  {
+    "revision": "ea622d31c9037424d9bfd0995175c986",
+    "url": "/WebDesignProject/static/media/electrician2.ea622d31.jpg"
+  },
+  {
+    "revision": "45878ff2adb59f8567d7a51fd8bb622d",
+    "url": "/WebDesignProject/static/media/electrician3.45878ff2.jpg"
+  },
+  {
+    "revision": "0e89f9ebd56b7adceb00f45eb9c4e8c5",
+    "url": "/WebDesignProject/static/media/electrician4.0e89f9eb.jpg"
+  },
+  {
+    "revision": "7c486b7bb4f195d4db54dd7add91561b",
+    "url": "/WebDesignProject/static/media/electrician5.7c486b7b.jpg"
+  },
+  {
+    "revision": "975644ef8f9ae8686ac7283a4e332a5b",
+    "url": "/WebDesignProject/static/media/electrician6.975644ef.jpg"
+  },
+  {
+    "revision": "7d4d90d6f465f2f095ccd4582229a65f",
+    "url": "/WebDesignProject/static/media/electronic1.7d4d90d6.jpg"
+  },
+  {
+    "revision": "60a9cb5ca9ff2f15f26827d92fcaa2e9",
+    "url": "/WebDesignProject/static/media/electronic2.60a9cb5c.jpg"
+  },
+  {
+    "revision": "715ca7c5dad69885135658fdbe0345b5",
+    "url": "/WebDesignProject/static/media/electronic3.715ca7c5.jpg"
+  },
+  {
+    "revision": "2c82bd817de2ee0d2610bd782413e63e",
+    "url": "/WebDesignProject/static/media/fitness1.2c82bd81.jpeg"
+  },
+  {
+    "revision": "cfd9901fb2ae8812f7622b3244c7d05d",
+    "url": "/WebDesignProject/static/media/fitness2.cfd9901f.jpg"
+  },
+  {
+    "revision": "09e050a6a5b726a9196dff35c97fedda",
+    "url": "/WebDesignProject/static/media/fitness22.09e050a6.jpg"
+  },
+  {
+    "revision": "a125940a37654dff15edc6bfc6e0ec36",
+    "url": "/WebDesignProject/static/media/fitness3.a125940a.jpg"
+  },
+  {
+    "revision": "f597b0cf46d5fa39e6865f937e644764",
+    "url": "/WebDesignProject/static/media/fitness3.f597b0cf.webp"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/WebDesignProject/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/WebDesignProject/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/WebDesignProject/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/WebDesignProject/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/WebDesignProject/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "ad893d1a9c55ae8afc99c8e2d6bc9fc9",
+    "url": "/WebDesignProject/static/media/h4.ad893d1a.png"
+  },
+  {
+    "revision": "b26fe90053adecbd609116d7cd77a928",
+    "url": "/WebDesignProject/static/media/hair-cut-scissors.b26fe900.jpg"
+  },
+  {
+    "revision": "6de11de28afe621d30f03a820ed8c3dc",
+    "url": "/WebDesignProject/static/media/hair-dryer.6de11de2.png"
+  },
+  {
+    "revision": "2b166bd09218308969e3f13d239b6acd",
+    "url": "/WebDesignProject/static/media/hairspa.2b166bd0.png"
+  },
+  {
+    "revision": "3b822f3c3ff186661326e0d5bbc3050e",
+    "url": "/WebDesignProject/static/media/home1.3b822f3c.jpg"
+  },
+  {
+    "revision": "97dca368b153c50e679dfc28f064c551",
+    "url": "/WebDesignProject/static/media/home2.97dca368.jpeg"
+  },
+  {
+    "revision": "ef6585e52320503f6118ab69ece7a0b8",
+    "url": "/WebDesignProject/static/media/home3.ef6585e5.jpeg"
+  },
+  {
+    "revision": "c78be94ce19854f272fae2c981593b16",
+    "url": "/WebDesignProject/static/media/icon1.c78be94c.png"
+  },
+  {
+    "revision": "7413a72ead49666b0c4b9b49ca9e6992",
+    "url": "/WebDesignProject/static/media/icon3.7413a72e.png"
+  },
+  {
+    "revision": "6473a43a2cce29eee23bc329f6d5a7fe",
+    "url": "/WebDesignProject/static/media/india.6473a43a.png"
+  },
+  {
+    "revision": "948fe9e1d21693f92b60f97e21d02918",
+    "url": "/WebDesignProject/static/media/jobs.948fe9e1.jpg"
+  },
+  {
+    "revision": "cc27450baaf09e38c6ae1c7da3de5e6e",
+    "url": "/WebDesignProject/static/media/makeupBg.cc27450b.jpg"
+  },
+  {
+    "revision": "84130af90fac81cf52132b3c345b31a0",
+    "url": "/WebDesignProject/static/media/massage.84130af9.jpg"
+  },
+  {
+    "revision": "b818d5cbcf41c119da465adf0a661327",
+    "url": "/WebDesignProject/static/media/mission.b818d5cb.png"
+  },
+  {
+    "revision": "3fdd60881922f6dcca4302c8a4d072ce",
+    "url": "/WebDesignProject/static/media/mk.3fdd6088.png"
+  },
+  {
+    "revision": "576a76f309b529d456ad24752ff093c6",
+    "url": "/WebDesignProject/static/media/mw.576a76f3.jpg"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/WebDesignProject/static/media/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/WebDesignProject/static/media/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/WebDesignProject/static/media/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/WebDesignProject/static/media/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/WebDesignProject/static/media/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "9b792b3c39dd65c0d38eb7efad21d5c9",
+    "url": "/WebDesignProject/static/media/pb.9b792b3c.jpg"
+  },
+  {
+    "revision": "709b05fedc45e961e316cf68b4247dbc",
+    "url": "/WebDesignProject/static/media/picture1.709b05fe.jpg"
+  },
+  {
+    "revision": "4d1324f4d1d7919f9fae2c94a8e24268",
+    "url": "/WebDesignProject/static/media/picture2.4d1324f4.jpg"
+  },
+  {
+    "revision": "9878e2a95976fad5283038cc61f6cf8e",
+    "url": "/WebDesignProject/static/media/profile.9878e2a9.jpg"
+  },
+  {
+    "revision": "77b25fd190b69bc2ab6ebc3ad427e1d1",
+    "url": "/WebDesignProject/static/media/profile1.77b25fd1.jpeg"
+  },
+  {
+    "revision": "ce8466534cab0e7a868127afbf700a63",
+    "url": "/WebDesignProject/static/media/profile3.ce846653.jpeg"
+  },
+  {
+    "revision": "d41ecac6c56e694d00a8eedc8c100485",
+    "url": "/WebDesignProject/static/media/profile4.d41ecac6.jpeg"
+  },
+  {
+    "revision": "894846b51afa31dc1515407f6e5a6997",
+    "url": "/WebDesignProject/static/media/random.894846b5.jpg"
+  },
+  {
+    "revision": "305b9078f2761709199f80da2c55371b",
+    "url": "/WebDesignProject/static/media/repair.305b9078.png"
+  },
+  {
+    "revision": "36780d8c8dffcc355970623e91627714",
+    "url": "/WebDesignProject/static/media/rf.36780d8c.jpg"
+  },
+  {
+    "revision": "178c3987be6f77dac8b2b488654269aa",
+    "url": "/WebDesignProject/static/media/salon1.178c3987.jpeg"
+  },
+  {
+    "revision": "4d4be0661269c3c69905ff1c4471486b",
+    "url": "/WebDesignProject/static/media/salon1.4d4be066.jpg"
+  },
+  {
+    "revision": "17d8053625e884384f1f3341a2720d8c",
+    "url": "/WebDesignProject/static/media/salon2.17d80536.jpg"
+  },
+  {
+    "revision": "7ea8266f0c3c92031a9a97a1644f528d",
+    "url": "/WebDesignProject/static/media/salon3.7ea8266f.jpg"
+  },
+  {
+    "revision": "4d00b8566988db1e9baf0dbd15abab74",
+    "url": "/WebDesignProject/static/media/sf.4d00b856.jpg"
+  },
+  {
+    "revision": "0d85069f900345829ec0286e59621410",
+    "url": "/WebDesignProject/static/media/spa.0d85069f.png"
+  },
+  {
+    "revision": "89902ccb23a810f5cdee7c511587055e",
+    "url": "/WebDesignProject/static/media/spa.89902ccb.jpg"
+  },
+  {
+    "revision": "4bbc0c8798da9c75780a0c6c803f7da2",
+    "url": "/WebDesignProject/static/media/tv.4bbc0c87.jpg"
+  },
+  {
+    "revision": "4711ef49a44f6ddc3f6533b25021e461",
+    "url": "/WebDesignProject/static/media/why_bg.4711ef49.png"
+  },
+  {
+    "revision": "4992a54c498755b6f9056e136c1e160c",
+    "url": "/WebDesignProject/static/media/wm.4992a54c.jpg"
+  },
+  {
+    "revision": "56b0d5bcf08ba0b831f18bad7f2f337b",
+    "url": "/WebDesignProject/static/media/wp.56b0d5bc.jpg"
+  },
+  {
+    "revision": "352619cb5b5a08a9f3bd23b4f7d893c9",
+    "url": "/WebDesignProject/static/media/yoga-mat.352619cb.png"
+  },
+  {
+    "revision": "0ce8cef50f5a9831dc78aafc4426652d",
+    "url": "/WebDesignProject/static/media/yoganew.0ce8cef5.png"
+  }
+]);
